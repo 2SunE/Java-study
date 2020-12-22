f5768e2bde8b2fe7dce279177a190af49966da98
2SunE <tjsrhkdjaak@gmail.com>
2020-12-23 00:33:49 +0900

    HashMap 예제

--- src/ch13/sec01/exam06/HashMapExample.java
+++ src/ch13/sec01/exam06/HashMapExample.java
@@ -11,13 +11,13 @@ public class HashMapExample {
 		map.put("홍길동", 85);
 		map.put("김철수", 95);
 		map.put("김영희", 80);
-		map.put("홍길동", 95); // "홍길동" 키가 같기 때문에 제일 마지막에 저장한 값으로 대체
+		map.put("홍길동", 93); // "홍길동" 키가 같기 때문에 제일 마지막에 저장한 값으로 대체
 		System.out.println("총 Entry 수: " + map.size());
 		
 		System.out.println("\t홍길동: " + map.get("홍길동")); // 이름(홍길동)으로 점수를 검색
 		System.out.println();
 		
-		
+		 
 	}
 
 }
