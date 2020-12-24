b14a9cbc46851271288d983ca072db68620749a6
2SunE <tjsrhkdjaak@gmail.com>
2020-12-24 22:47:15 +0900

    상속(inheritance) 예제

---
+++ src/ch07/sec01/exam01/CellPhone.java
@@ -0,0 +1,13 @@
+package ch07.sec01.exam01;
+
+public class CellPhone {
+	String model;
+	String color;
+	
+	void powerOn() {
+		System.out.println("전원을 켭니다");
+	}
+	void powerOff() {
+		System.out.println("전원을 끕니다.");
+	}
+}
