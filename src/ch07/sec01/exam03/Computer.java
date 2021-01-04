7c00b70b2c62462284861f5655550064868f386d
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 02:38:44 +0900

    Update Computer.java (Calculator 자식클래스)

--- src/ch07/sec01/exam03/Computer.java
+++ src/ch07/sec01/exam03/Computer.java
@@ -2,6 +2,7 @@ package ch07.sec01.exam03;
 
 public class Computer extends Calculator{
 	
+	// 메소드 재정의 (Method Overriding)
 	@Override
 	double areaCircle(double r) {
 		System.out.println("Computer 객체의 areaCircle() 실행");
